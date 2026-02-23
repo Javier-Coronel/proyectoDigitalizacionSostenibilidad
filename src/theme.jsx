4a6d68635569cd7842a74cf0d9026cf87d1af7af
Javier-Coronel <90841478+Javier-Coronel@users.noreply.github.com>
2026-02-23 12:49:10 +0100

    created dist

--- src/theme.jsx
+++
@@ -1,9 +0,0 @@
-import { createTheme } from '@mui/material/styles';
-const theme = createTheme({
-    palette: {
-    primary:{ main:"#ffe6c3"},
-    light: '#fff3e1',
-  },
-    
-})
-export default theme;
